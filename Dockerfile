55d92fad94a917efbac4bcbd5009add0d85677e0
primasghar <mirpasghar@gmail.com>
2024-09-13 17:12:48 +0300

    Updated docker file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM denoland/deno:alpine-1.42.2
+FROM denoland/deno:alpine-1.46.3
 
 EXPOSE 7777
 
--- deps.js
+++ deps.js
@@ -1,3 +1,2 @@
 export { serve } from "https://deno.land/std@0.222.1/http/server.ts";
 export { configure, renderFile } from "https://deno.land/x/eta@v2.2.0/mod.ts";
-import * as eta from "https://deno.land/x/eta@v2.2.0/err.ts";
